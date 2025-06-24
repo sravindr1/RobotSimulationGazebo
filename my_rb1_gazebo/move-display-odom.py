3f8d3e3d25d2b004e915732bb73c78ba5279cbac
Sarala <sravindr@mtu.edu>
2025-06-24 04:06:37 +0000

    add driver control and laserplugins

---
+++ my_rb1_gazebo/move-display-odom.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+def callback(msg):
+    print('X: %s, Y: %s' % (msg.pose.pose.position.x, msg.pose.pose.position.y))
+
+def main():
+    sub = rospy.Subscriber('/odom', Odometry, callback)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('exercise_node', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        msg = Twist()
+        msg.angular.z = 0.3
+
+        msg.linear.x = 0.3
+
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
