4dd05975e8d870aab6043c88db7416fe1b1371cc
sravindr1 <sravindr@mtu.edu>
2025-06-26 05:57:58 +0000

    robot_move

--- my_rb1_gazebo/move-display-odom.py
+++ my_rb1_gazebo/move-display-odom.py
@@ -5,20 +5,19 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
 def callback(msg):
-    print('X: %s, Y: %s' % (msg.pose.pose.position.x, msg.pose.pose.position.y))
+    # print('X: %s, Y: %s' % (msg.pose.pose.position.x, msg.pose.pose.position.y))
+    print("yes")
 
 def main():
-    sub = rospy.Subscriber('/odom', Odometry, callback)
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    sub = rospy.Subscriber('/cmd_vel', Twist, callback)
+    #pub = rospy.Publisher('/odom', Odometry, queue_size=10)
     rospy.init_node('exercise_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        msg = Twist()
-        msg.angular.z = 0.3
-
-        msg.linear.x = 0.3
-
-        pub.publish(msg)
+    #     msg = Twist()
+    #     msg.angular.z = 0.3
+    #     msg.linear.x = 0.3
+    #     pub.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
